b88f78be081edd8a1db3e18397ff704744d0224d
OJ Abba <phavorabba@gmail.com>
2023-02-08 00:09:35 +0000

    getCountryName Function Create

--- app/src/main/java/com/havorstudios/troftey/MainActivity.java
+++ app/src/main/java/com/havorstudios/troftey/MainActivity.java
@@ -2,7 +2,20 @@ package com.havorstudios.troftey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import android.location.Address;
+import android.location.Geocoder;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +25,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    // Final est
+    public String getCountryName(double lat, double lng) {
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(lat, lng, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (addresses != null && addresses.size() > 0) {
+            return addresses.get(0).getCountryName();
+        }
+        return "";
+    }
+
 }
